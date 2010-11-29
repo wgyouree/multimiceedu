5677bbf6d551a7211df35cc6b707b49456093d0d
wgyouree@gmail.com <wgyouree@gmail.com@b7037a0f-6d88-4577-5e2c-27c513b299a2>
2010-11-29 01:34:03 +0000

    - top left resize is working, but there is a white border artifact the size of the original window area - resize handles should be larger, they are hard to click - still need to get the various cursors appearing when the resize handles are moused over

---
+++ MT4j/src/cet/globalMultiMiceManager/Size.java
@@ -0,0 +1,28 @@
+package cet.globalMultiMiceManager;
+
+public class Size {
+
+	private float width;
+	private float height;
+	
+	public Size(float width, float height) {
+		this.width = width;
+		this.height = height;
+	}
+
+	public float getWidth() {
+		return width;
+	}
+
+	public void setWidth(float width) {
+		this.width = width;
+	}
+
+	public float getHeight() {
+		return height;
+	}
+
+	public void setHeight(float height) {
+		this.height = height;
+	}
+}
